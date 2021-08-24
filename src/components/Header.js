da453ec4151c7659bf39fd60db0f09718cc63d52
RamSaravanaRaja <rramsaravana@gmail.com>
2021-08-24 05:46:59 +0530

    updated with minor changes

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1>The<strong>Anime-World</strong>Collection</h1>
+        </header>
+    )
+}
+
+export default Header
